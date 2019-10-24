38efc55d5a6bb4728e1c5f76d6d0c62ed68bb1d9
Michael Lipscombe <mlipscombe@outlook.com>
2019-10-24 11:53:09 -0600

    added new env

--- scripts/deploy-control-plane.sh
+++ scripts/deploy-control-plane.sh
@@ -19,17 +19,17 @@ private_key=${PWD}/control-plane/state/${ENVIRONMENT_NAME}/sshkey
 export BOSH_ALL_PROXY="ssh+socks5://ubuntu@${OM_opsman_host_name}:22?private-key=${private_key}"
 bosh login
 
- echo "Upload Stemcells & Releases..."
- pushd ./downloads > /dev/null
-   bosh upload-stemcell bosh-stemcell-*.tgz
-   bosh upload-release concourse-bosh-release-*.tgz
-   bosh upload-release bosh-dns-aliases-release-*.tgz
-   bosh upload-release bpm-release-*.tgz
-   bosh upload-release credhub-release-*.tgz
-   bosh upload-release postgres-release-*.tgz
-   bosh upload-release garden-runc-release-*.tgz
-   bosh upload-release uaa-release-*.tgz
- popd > /dev/null
+# echo "Upload Stemcells & Releases..."
+# pushd ./downloads > /dev/null
+#   bosh upload-stemcell bosh-stemcell-*.tgz
+#   bosh upload-release concourse-bosh-release-*.tgz
+#   bosh upload-release bosh-dns-aliases-release-*.tgz
+#   bosh upload-release bpm-release-*.tgz
+#   bosh upload-release credhub-release-*.tgz
+#   bosh upload-release postgres-release-*.tgz
+#   bosh upload-release garden-runc-release-*.tgz
+#   bosh upload-release uaa-release-*.tgz
+# popd > /dev/null
 
 export CP_ca_cert=$(om --env /tmp/env.yml certificate-authorities --format json | jq -r '.[0] | select(.active==true) | .cert_pem' )
 
