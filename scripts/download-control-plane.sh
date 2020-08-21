6019bcd972995d911a377cd09c186d9ba4b5e921
Mark Alston <malston@vmware.com>
2020-08-21 10:52:03 -0600

    Add increased-disk vm extension

--- scripts/download-control-plane.sh
+++ scripts/download-control-plane.sh
@@ -14,11 +14,9 @@ pushd "${base_dir}/downloads" > /dev/null
   pivnet login --api-token "$PIVNET_TOKEN"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "concourse-bosh-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "concourse-bosh-deployment-*.tgz"
-  # # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "garden-runc-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "postgres-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "credhub-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "uaa-release-*.tgz"
-  # # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "bosh-dns-aliases-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "bpm-release-*.tgz"
   pivnet download-product-files -p stemcells-ubuntu-xenial -r "${concourse_stemcell_version}" -g "bosh-stemcell-*-vsphere-*.tgz"
   pivnet logout
