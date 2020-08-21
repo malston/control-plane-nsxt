accfa82b5244fbef3ef92521acdacb630727c985
Mark Alston <malston@vmware.com>
2020-08-21 10:16:59 -0600

    Use concourse-bosh-deployments repo for concourse now

--- scripts/deploy-control-plane.sh
+++ scripts/deploy-control-plane.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-
-echo "Obtain bosh credentials"
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="${__DIR}/.."
 
 export OM_opsman_host_name=$(bosh int ./control-plane/vars/${ENVIRONMENT_NAME}/opsman.yml --path /opsman_host_name)
 export OM_opsman_user=$OPSMAN_USER
@@ -17,22 +17,38 @@ eval "$(om --env /tmp/env.yml bosh-env)"
 
 private_key=${PWD}/control-plane/state/${ENVIRONMENT_NAME}/sshkey
 export BOSH_ALL_PROXY="ssh+socks5://ubuntu@${OM_opsman_host_name}:22?private-key=${private_key}"
+export CREDHUB_PROXY=$BOSH_ALL_PROXY
+credhub login
+
+export ADMIN_USERNAME=admin
+export ADMIN_PASSWORD=password
+
+credhub set \
+   -n /p-bosh/concourse/local_user \
+   -t user \
+   -z "${ADMIN_USERNAME}" \
+   -w "${ADMIN_PASSWORD}"
+
+# terraform output -state=${base_dir}/control-plane/state/${ENVIRONMENT_NAME}/terraform.tfstate stable_config > /tmp/terraform-outputs.yml
+
+# export CONCOURSE_URL="$(terraform output -state=${base_dir}/control-plane/state/${ENVIRONMENT_NAME}/terraform.tfstate concourse_url)"
+
 bosh login
 
-# echo "Upload Stemcells & Releases..."
-# pushd ./downloads > /dev/null
-#   bosh upload-stemcell bosh-stemcell-*.tgz
-#   bosh upload-release concourse-bosh-release-*.tgz
-#   bosh upload-release bosh-dns-aliases-release-*.tgz
-#   bosh upload-release bpm-release-*.tgz
-#   bosh upload-release credhub-release-*.tgz
-#   bosh upload-release postgres-release-*.tgz
-#   bosh upload-release garden-runc-release-*.tgz
-#   bosh upload-release uaa-release-*.tgz
-# popd > /dev/null
-
-# export CP_ca_cert=$(om --env /tmp/env.yml certificate-authorities --format json | jq -r '.[0] | select(.active==true) | .cert_pem' )
-
-bosh deploy --non-interactive -d control-plane ./control-plane/manifests/concourse/4.2.4/control-plane.yml \
-    --vars-file=./control-plane/vars/${ENVIRONMENT_NAME}/control-plane.yml
-#    --vars-env CP
+bosh -n -d concourse deploy "${base_dir}/downloads/concourse-bosh-deployment/cluster/concourse.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/privileged-http.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/privileged-https.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/basic-auth.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/tls-vars.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/tls.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/uaa.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/credhub-colocated.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/offline-releases.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/backup-atc-colocated-web.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/secure-internal-postgres.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/secure-internal-postgres-bbr.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/secure-internal-postgres-uaa.yml" \
+  -o "${base_dir}/downloads/concourse-bosh-deployment/cluster/operations/secure-internal-postgres-credhub.yml" \
+  -o operations.yml \
+  -l "${base_dir}/control-plane/vars/${ENVIRONMENT_NAME}/concourse.yml" \
+  -l "${base_dir}/downloads/concourse-bosh-deployment/versions.yml"
--- scripts/download-control-plane.sh
+++ scripts/download-control-plane.sh
@@ -2,22 +2,33 @@
 
 set -eu
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="${__DIR}/.."
+
 concourse_stemcell_version=$(om interpolate --config ./versions.yml --path /concourse_stemcell_version)
 concourse_version=$(om interpolate --config ./versions.yml --path /concourse_version)
 
-mkdir -p ./downloads
+mkdir -p "${base_dir}/downloads"
 
-pushd ./downloads > /dev/null
+pushd "${base_dir}/downloads" > /dev/null
   pivnet login --api-token "$PIVNET_TOKEN"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "concourse-bosh-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "concourse-bosh-deployment-*.tgz"
-  # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "garden-runc-release-*.tgz"
+  # # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "garden-runc-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "postgres-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "credhub-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "uaa-release-*.tgz"
-  # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "bosh-dns-aliases-release-*.tgz"
+  # # pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "bosh-dns-aliases-release-*.tgz"
   pivnet download-product-files -p p-concourse -r "${concourse_version}" -g "bpm-release-*.tgz"
   pivnet download-product-files -p stemcells-ubuntu-xenial -r "${concourse_stemcell_version}" -g "bosh-stemcell-*-vsphere-*.tgz"
   pivnet logout
 popd > /dev/null
 
+rm -rf "${base_dir}/downloads/concourse-bosh-deployment/"
+mkdir -p "${base_dir}/downloads/concourse-bosh-deployment"
+tar -zxvf ${base_dir}/downloads/concourse-bosh-deployment-*.tgz --strip=1 -C "${base_dir}/downloads/concourse-bosh-deployment"
+
+version=$(echo "${concourse_version}" | cut -d' ' -f 1)
+
+mkdir -p "${base_dir}/control-plane/manifests/concourse/${version}"
+cp "${base_dir}/downloads/concourse-bosh-deployment/cluster/concourse.yml" "${base_dir}/control-plane/manifests/concourse/${version}/concourse.yml"
